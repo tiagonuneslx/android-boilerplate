9a1a2394e916fcb40e1ccb6326630b7c8604c611
tiagonuneslx <tiago.nunes.lx@gmail.com>
2020-12-25 03:10:51 +0000

    Main Activity created

---
+++ app/src/main/java/com/example/androidboilerplate/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidboilerplate.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidboilerplate.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
