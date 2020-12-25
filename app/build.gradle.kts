7942b6b3fab2599fb0adb7097bcf76758375b622
tiagonuneslx <tiago.nunes.lx@gmail.com>
2020-12-25 03:32:54 +0000

    Updated Activity and Fragment libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,10 +25,22 @@ android {
 }
 
 dependencies {
+
+    // Kotlin
     implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
     implementation("androidx.core:core-ktx:1.3.2")
+
+    // AppCompat
     implementation("androidx.appcompat:appcompat:1.2.0")
+
+    // Material Design
     implementation("com.google.android.material:material:1.2.1")
+
+    // Activity & Fragment
+    implementation("androidx.activity:activity-ktx:1.2.0-rc01")
+    implementation("androidx.fragment:fragment-ktx:1.3.0-rc01")
+
+    // Testing
     testImplementation("junit:junit:4.13.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
