63b1087a597c2a5cc41c13fe5adf66e6c95494bf
tiagonuneslx <tiago.nunes.lx@gmail.com>
2020-12-25 03:46:59 +0000

    Added Kapt and Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion
 plugins {
     id("com.android.application")
     kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -16,10 +18,18 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
     buildTypes {
         getByName("release") {
             minifyEnabled(false)
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
@@ -39,6 +49,12 @@ dependencies {
     // Activity & Fragment
     implementation("androidx.activity:activity-ktx:1.2.0-rc01")
     implementation("androidx.fragment:fragment-ktx:1.3.0-rc01")
+
+    // Hilt - Dependency Injection
+    implementation("com.google.dagger:hilt-android:2.28-alpha")
+    kapt("com.google.dagger:hilt-android-compiler:2.28-alpha")
+    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha02")
+    kapt("androidx.hilt:hilt-compiler:1.0.0-alpha02")
 
     // Testing
     testImplementation("junit:junit:4.13.1")
--- app/src/main/java/com/example/androidboilerplate/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidboilerplate/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.androidboilerplate.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidboilerplate.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
